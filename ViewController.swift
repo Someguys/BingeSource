e5a849324d3a89051733c2a953d14d723d9cd10a
jon-greenec3 <jon.greenec3@gmail.com>
2016-03-03 19:09:17 -0600

    Create ViewController.swift

---
+++ ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  BingeSource
+//
+//  Created by Windell Greene on 3/2/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tvShowTitle: UILabel!
+    
+    @IBOutlet weak var tvShowImageView: UIImageView!
+    
+    @IBOutlet weak var tvShowDescription: UILabel!
+    
+    @IBOutlet weak var moreDescriptionButton: UIButton!
+    
+    @IBOutlet weak var availableOnLabel: UILabel!
+    
+    @IBOutlet weak var serviceImageViewAlpha: UIImageView!
+    
+    @IBOutlet weak var serviceImageViewBeta: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
